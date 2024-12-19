055d2dd918ed0d9598d2a538dd6d6978b5fd0ff7
Rodrigo Gutierrez Cruz <134517998+ItzRodri@users.noreply.github.com>
2024-12-19 14:39:31 -0400

    deleted bug

--- entregas-bo/src/app/page.tsx
+++ entregas-bo/src/app/page.tsx
@@ -4,18 +4,13 @@ import NavBar from "./NavBar";
 import Image from "next/image";
 import HomePageHeader from "./Sections/HomePage/home-page-header";
 import HomePageSponsors from "./Sections/HomePage/home-page-sponsors";
-import LottieAnimation from "./Sections/HomePage/home-page-categories";
+
 export default function Home() {
   return (
     <>
       <HomePageHeader/>
       
-      <LottieAnimation
-        animationData='/lacteos.json'
-        loop={true}
-        autoplay={true}
-        className="w-96 h-96"
-      />
+
      <HomePageSponsors/>
     </>
   );
