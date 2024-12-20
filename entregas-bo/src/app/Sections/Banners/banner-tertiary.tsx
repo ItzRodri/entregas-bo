128b3744a3a8885959cf4516b4852f51f6694fad
Kobe Bryan Gonzales Balderrama <kobebgonzalesb@gmail.com>
2024-12-20 12:15:16 -0400

    asd

--- entregas-bo/src/app/Sections/Banners/banner-tertiary.tsx
+++ entregas-bo/src/app/Sections/Banners/banner-tertiary.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Image from "next/image";
 
-const HeroBannerSecondary: React.FC = () => {
+const HeroBannerTertiary: React.FC = () => {
   return (
-<div className="container mx-auto my-10 w-[90%] md:w-[95%] px-6 md:px-8 py-10 relative flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-[#FCCF42] to-[#007D3D] text-white rounded-lg overflow-hidden">
+<div className="container mx-auto my-10 w-[90%] md:w-[95%] px-6 md:px-8 py-6 relative flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-[#E44F46] to-[#5FC1E7] text-white rounded-lg overflow-hidden">
   {/* Contenido de Texto */}
   <div className="z-10 space-y-4 md:space-y-6 animate-fade-in-left duration-700 delay-200">
-    <h1 className="text-3xl md:text-5xl font-bold">DESCUBRE NUESTRAS OFERTAS</h1>
-    <p className="text-sm md:text-lg">¡Precios increíbles para ti!</p>
-    <button className="bg-[#E44F46] text-white px-6 py-3 rounded-md hover:bg-[#ff00007a] transition duration-300">
-      Ver Ofertas
+    <h1 className="text-3xl md:text-5xl font-bold">¡NOVEDADES EXCLUSIVAS!</h1>
+    <p className="text-sm md:text-lg">Encuentra productos únicos.</p>
+    <button className="bg-[#FCCF42] text-white px-6 py-3 rounded-md hover:bg-[#ffd34a] transition duration-300">
+      Descúbrelo ahora
     </button>
   </div>
 
@@ -17,21 +17,21 @@ const HeroBannerSecondary: React.FC = () => {
   <div className="w-full md:w-1/2 flex justify-evenly items-center mt-6 md:mt-0">
     <Image
       src="/libros/Actividades-de-M1.png"
-      alt="Libro 1"
+      alt="Libro 4"
       width={150}
       height={200}
       className=""
     />
     <Image
       src="/libros/Actividades-de-M1.png"
-      alt="Libro 2"
+      alt="Libro 5"
       width={150}
       height={200}
       className=""
     />
     <Image
       src="/libros/Actividades-de-M1.png"
-      alt="Libro 3"
+      alt="Libro 6"
       width={150}
       height={200}
       className=""
@@ -43,4 +43,4 @@ const HeroBannerSecondary: React.FC = () => {
   );
 };
 
-export default HeroBannerSecondary;
+export default HeroBannerTertiary;
--- entregas-bo/src/app/page.tsx
+++ entregas-bo/src/app/page.tsx
@@ -4,6 +4,8 @@ import ClientWrapper from "./Sections/HomePage/client-wrapper";
 import Blogs from "./Sections/HomePage/home-page-blogs";
 import ServicePage from "./Sections/HomePage/home-page-servicepage";
 import HeroBannerPrimary from "./Sections/Banners/banner-primary";
+import HeroBannerSecondary from "./Sections/Banners/banner-secondary";
+import HeroBannerTertiary from "./Sections/Banners/banner-tertiary";
 
 export default function Home() {
   return (
@@ -11,8 +13,8 @@ export default function Home() {
       <HeroBannerPrimary/>
       <ClientWrapper />
       <HomePageSponsors/>
-      <HeroBannerPrimary/>
-      <HeroBannerPrimary/>
+      <HeroBannerSecondary/>
+      <HeroBannerTertiary/>
       <Blogs/>
       <ServicePage/>
     </>
