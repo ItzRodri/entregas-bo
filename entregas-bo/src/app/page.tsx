5c985288827294348aeaa5fe636b41d45852a111
Kobe Bryan Gonzales Balderrama <kobebgonzalesb@gmail.com>
2024-12-23 08:53:34 -0400

    asd

--- entregas-bo/src/app/page.tsx
+++ entregas-bo/src/app/page.tsx
@@ -3,9 +3,9 @@ import HomePageSponsors from "./Sections/HomePage/home-page-sponsors";
 import ClientWrapper from "./Sections/HomePage/client-wrapper";
 import Blogs from "./Sections/HomePage/home-page-blogs";
 import ServicePage from "./Sections/HomePage/home-page-servicepage";
-import HeroBannerPrimary from "./Sections/Banners/banner-primary";
-import HeroBannerSecondary from "./Sections/Banners/banner-secondary";
-import HeroBannerTertiary from "./Sections/Banners/banner-tertiary";
+import HeroBannerPrimary from "./components/banner-primary";
+import HeroBannerSecondary from "./components/banner-secondary";
+import HeroBannerTertiary from "./components/banner-tertiary";
 
 export default function Home() {
   return (
