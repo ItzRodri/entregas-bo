39a51768419ff991c74bad5be08e45e06d48a596
Kobe Bryan Gonzales Balderrama <kobebgonzalesb@gmail.com>
2024-12-25 16:34:56 -0400

    Correccion de bugs

--- entregas-bo/src/app/Sections/Profile/PersonalData/personaldata.tsx
+++ entregas-bo/src/app/Sections/Profile/PersonalData/personaldata.tsx
@@ -3,14 +3,12 @@ import CustomInput from "@/app/components/common/inputprops";
 import EditableModal from "@/app/components/common/modalprops";
 
 const PersonalData: React.FC = () => {
-  // State para manejar si el modal está abierto o cerrado
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalFields, setModalFields] = useState<
     { label: string; placeholder: string; value: string; type?: string }[]
   >([]);
   const [modalTitle, setModalTitle] = useState("");
 
-  // State para almacenar los datos personales
   const [personalData, setPersonalData] = useState({
     nombre: "Jimmy Smith",
     email: "Jimmy.smith1996@gmail.com",
@@ -20,72 +18,63 @@ const PersonalData: React.FC = () => {
     codigopostal: "02139",
   });
 
-  // Función para manejar la edición de los campos
-  const handleEdit = (field: string) => {
-    const fieldMapping: {
-      [key: string]: {
-        label: string;
-        placeholder: string;
-        value: string;
-        type?: string;
-      }[];
-    } = {
-      "Nombre completo": [
-        {
-          label: "Nombre",
-          placeholder: "Nombre",
-          value: personalData.nombre.split(" ")[0],
-        },
-        {
-          label: "Apellido",
-          placeholder: "Apellido",
-          value: personalData.nombre.split(" ")[1] || "",
-        },
-      ],
-      "Correo electrónico": [
-        {
-          label: "Email",
-          placeholder: "Dirección de correo electrónico",
-          value: personalData.email,
-        },
-      ],
-      Teléfono: [
-        {
-          label: "Teléfono",
-          placeholder: "Número de teléfono",
-          value: personalData.telefono,
-        },
-      ],
-      Contraseña: [
-        {
-          label: "Contraseña",
-          placeholder: "Nueva contraseña",
-          value: "",
-          type: "password",
-        },
-      ],
-      Dirección: [
-        {
-          label: "Dirección",
-          placeholder: "Dirección",
-          value: personalData.direccion,
-        },
-      ],
-      "Código postal": [
-        {
-          label: "Código Postal",
-          placeholder: "Código postal",
-          value: personalData.codigopostal,
-        },
-      ],
-    };
+  const fieldMapping = {
+    "Nombre completo": [
+      {
+        label: "Nombre",
+        placeholder: "Nombre",
+        value: personalData.nombre.split(" ")[0],
+      },
+      {
+        label: "Apellido",
+        placeholder: "Apellido",
+        value: personalData.nombre.split(" ")[1] || "",
+      },
+    ],
+    "Correo electrónico": [
+      {
+        label: "Email",
+        placeholder: "Dirección de correo electrónico",
+        value: personalData.email,
+      },
+    ],
+    Teléfono: [
+      {
+        label: "Teléfono",
+        placeholder: "Número de teléfono",
+        value: personalData.telefono,
+      },
+    ],
+    Contraseña: [
+      {
+        label: "Contraseña",
+        placeholder: "Nueva contraseña",
+        value: "",
+        type: "password",
+      },
+    ],
+    Dirección: [
+      {
+        label: "Dirección",
+        placeholder: "Dirección",
+        value: personalData.direccion,
+      },
+    ],
+    "Código postal": [
+      {
+        label: "Código Postal",
+        placeholder: "Código postal",
+        value: personalData.codigopostal,
+      },
+    ],
+  };
 
+  const handleEdit = (field: keyof typeof fieldMapping) => { // Tipar `field`
     setModalFields(fieldMapping[field] || []);
     setModalTitle(field);
     setIsModalOpen(true);
   };
 
-  // Función para manejar el guardado de los valores del modal
   const handleModalSave = (updatedValues: { [key: string]: string }) => {
     setPersonalData((prev) => ({
       ...prev,
@@ -99,31 +88,34 @@ const PersonalData: React.FC = () => {
       <div className="flex-grow p-6">
         <h2 className="text-xl font-semibold text-gray-800">Identificación</h2>
         <p className="text-sm text-gray-500 mb-6">Verifica tu identidad</p>
-        {/* Grilla responsiva */}
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4">
           <CustomInput
-            placeholder="Nombre completo"
+            label="Nombre completo"
+            placeholder="Ingrese su nombre"
             value={personalData.nombre}
             icon="/icons/user.svg"
             editable={true}
             onEdit={() => handleEdit("Nombre completo")}
           />
           <CustomInput
-            placeholder="Dirección de correo electrónico"
+            label="Dirección de correo electrónico"
+            placeholder="Ingrese su correo electrónico"
             value={personalData.email}
             icon="/icons/email.svg"
             editable={true}
             onEdit={() => handleEdit("Correo electrónico")}
           />
           <CustomInput
-            placeholder="Número de teléfono"
+            label="Número de teléfono"
+            placeholder="Ingrese su número de teléfono"
             value={personalData.telefono}
             icon="/icons/phone.svg"
             editable={true}
             onEdit={() => handleEdit("Teléfono")}
           />
           <CustomInput
-            placeholder="Contraseña"
+            label="Contraseña"
+            placeholder="Ingrese su contraseña"
             value={personalData.contraseña}
             icon="/icons/key.svg"
             editable={true}
@@ -131,14 +123,16 @@ const PersonalData: React.FC = () => {
             onEdit={() => handleEdit("Contraseña")}
           />
           <CustomInput
-            placeholder="Dirección"
+            label="Dirección"
+            placeholder="Ingrese su dirección"
             value={personalData.direccion}
             icon="/icons/home.svg"
             editable={true}
             onEdit={() => handleEdit("Dirección")}
           />
           <CustomInput
-            placeholder="Código postal"
+            label="Código postal"
+            placeholder="Ingrese su código postal"
             value={personalData.codigopostal}
             icon="/icons/postalcode.svg"
             editable={true}
@@ -147,7 +141,6 @@ const PersonalData: React.FC = () => {
         </div>
       </div>
 
-     {/* Modal para editar los campos */}
       {isModalOpen && (
         <EditableModal
           title={modalTitle}
--- entregas-bo/src/app/components/common/inputprops.tsx
+++ entregas-bo/src/app/components/common/inputprops.tsx
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Image from "next/image";
 
 // props del input
 interface CustomInputProps {
+  label: string; // Nuevo prop para la etiqueta
   placeholder: string;
   value: string;
   icon: string;
   editable: boolean;
   type?: string;
-  onEdit: () => void; // para manejar la edicion
+  onEdit: () => void; // para manejar la edición
   onChange?: (newValue: string) => void; // Nueva prop para manejar cambios
 }
 
-
-const CustomInput: React.FC<CustomInputProps> = ({ placeholder, value, icon, editable, type = "text", onEdit, onChange }) => {
-  
+const CustomInput: React.FC<CustomInputProps> = ({
+  label,
+  placeholder,
+  value,
+  icon,
+  editable,
+  type = "text",
+  onEdit,
+  onChange,
+}) => {
   const [isPasswordVisible, setPasswordVisible] = useState(false);
 
   // Función para cambiar la visibilidad de la contraseña
@@ -23,57 +31,66 @@ const CustomInput: React.FC<CustomInputProps> = ({ placeholder, value, icon, edi
   };
 
   return (
-    <div className="flex items-center border border-gray-300 rounded-md p-2">
-      {icon && (
-        <Image
-          src={icon}
-          alt="Input Icon"
-          width={24}
-          height={24}
-          className="mr-3 filter-gray"
-        />
-      )}
-      <div className="flex-grow min-w-0">
-        <p className="text-xs text-gray-500 truncate">{placeholder}</p>
-        <input
-          type={type === "password" && !isPasswordVisible ? "password" : "text"}
-          value={value}
-          readOnly={!editable}
-          onChange={(e) => onChange && onChange(e.target.value)} // Llamada al manejador de cambios
-          className="text-sm font-medium text-gray-800 truncate bg-transparent outline-none w-full"
-        />
+    <div className="flex flex-col">
+      {/* Etiqueta fuera del input */}
+      <label
+        className="text-sm font-medium text-gray-600 mb-1"
+        style={{ color: "#717171" }}
+      >
+        {label}
+      </label>
+      <div className="flex items-center border border-gray-300 rounded-md p-2 bg-gray-50">
+        {icon && (
+          <Image
+            src={icon}
+            alt="Input Icon"
+            width={24}
+            height={24}
+            className="mr-3 filter-gray"
+          />
+        )}
+        <div className="flex-grow min-w-0">
+          <input
+            type={type === "password" && !isPasswordVisible ? "password" : "text"}
+            value={value}
+            placeholder={editable ? placeholder : value}
+            readOnly={!editable}
+            onChange={(e) => onChange && onChange(e.target.value)}
+            className="text-sm font-medium truncate bg-transparent outline-none w-full"
+            style={{ color: "#717171" }}
+          />
+        </div>
+        {editable && type === "password" && (
+          <button
+            onClick={togglePasswordVisibility}
+            className="ml-3 flex-shrink-0 text-blue-500 hover:text-blue-700"
+            aria-label="Toggle Password Visibility"
+          >
+            <Image
+              src={isPasswordVisible ? "/icons/eye-close.svg" : "/icons/eye.svg"}
+              alt="Toggle Password Visibility"
+              width={20}
+              height={20}
+            />
+          </button>
+        )}
+        {editable && onEdit && (
+          <button
+            onClick={onEdit}
+            className="ml-3 flex-shrink-0 text-blue-500 hover:text-blue-700"
+            aria-label="Edit"
+          >
+            <Image
+              src="/icons/edit.svg"
+              alt="Edit Icon"
+              width={20}
+              height={20}
+            />
+          </button>
+        )}
       </div>
-      {editable && type === "password" && (
-        <button
-          onClick={togglePasswordVisibility}
-          className="ml-3 flex-shrink-0 text-blue-500 hover:text-blue-700"
-          aria-label="Toggle Password Visibility"
-        >
-          <Image
-            src={isPasswordVisible ? "/icons/eye-close.svg" : "/icons/eye.svg"}
-            alt="Toggle Password Visibility"
-            width={20}
-            height={20}
-          />
-        </button>
-      )}
-      {editable && onEdit && (
-        <button
-          onClick={onEdit}
-          className="ml-3 flex-shrink-0 text-blue-500 hover:text-blue-700"
-          aria-label="Edit"
-        >
-          <Image
-            src="/icons/edit.svg"
-            alt="Edit Icon"
-            width={20}
-            height={20}
-          />
-        </button>
-      )}
     </div>
   );
 };
-
 
 export default CustomInput;
